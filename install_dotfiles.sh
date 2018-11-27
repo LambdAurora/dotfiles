6ea136ac028cf5697e68ee22c738adc6f0ba967c
LambdAurora <aurora42lambda@gmail.com>
2018-11-27 16:53:08 +0100

    :sparkles: Added i3 and polybar configuration files.

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -5,16 +5,30 @@ echo "Installing dotfiles..."
 # Get current dir
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Install
-echo "Installing .zshrc..."
-ln -svf "$DOTFILES_DIR/.zshrc" ~
-echo "Installing .lambdadotfiles/..."
+## Installation.
+
+echo "Installing common dotfiles..."
+mkdir -p ~/.config
 ln -svf "$DOTFILES_DIR/.lambdadotfiles/" ~
-echo "Installing oh-my-zsh files..."
+
+# Install configuration files for the shell.
+echo "Installing zsh configuration files..."
+ln -svf "$DOTFILES_DIR/.zshrc" ~
 mkdir -p ~/.oh-my-zsh/custom/themes
 ln -svf "$DOTFILES_DIR/.oh-my-zsh/custom/themes/lambdaurora.zsh-theme" ~/.oh-my-zsh/custom/themes
-echo "Installing .vimrc..."
+
+# Install configuration files for vim.
+echo "Installing vim configuration files..."
 ln -svf "$DOTFILES_DIR/.vimrc" ~
-echo "Installing .vim files..."
 mkdir -p ~/.vim/colors
 ln -svf "$DOTFILES_DIR/.vim/colors/hybrid.vim" ~/.vim/colors
+
+# Install configuration files for i3.
+echo "Installing i3 configuration files..."
+mkdir -p ~/.config/i3
+ln -svf "$DOTFILES_DIR/.config/i3/config" ~/.config/i3
+
+# Install configuration files for polybar.
+mkdir -p ~/.config/polybar
+ln -svf "$DOTFILES_DIR/.config/polybar/config" ~/.config/polybar
+ln -svf "$DOTFILES_DIR/.config/polybar/launch.sh" ~/.config/polybar
