88d069206eaa6e621d17e5e8e5a83756607f56a0
LambdAurora <aurora42lambda@gmail.com>
2018-11-29 21:21:54 +0100

    :sparkles: Added --verbose argument to install_dotfiles script.

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -4,31 +4,41 @@ echo "Installing dotfiles..."
 
 # Get current dir
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+alias tmp_ln="ln -sf"
+for param in "$*"
+do
+	if [[ $param == "--verbose" ]] || [[ $param == "-v" ]]
+	then
+		alias tmp_ln="ln -svf"
+	fi
+done
 
 ## Installation.
 
 echo "Installing common dotfiles..."
 mkdir -p ~/.config
-ln -svf "$DOTFILES_DIR/.lambdadotfiles/" ~
+tmp_ln "$DOTFILES_DIR/.lambdadotfiles/" ~
 
 # Install configuration files for the shell.
 echo "Installing zsh configuration files..."
-ln -svf "$DOTFILES_DIR/.zshrc" ~
+tmp_ln "$DOTFILES_DIR/.zshrc" ~
 mkdir -p ~/.oh-my-zsh/custom/themes
-ln -svf "$DOTFILES_DIR/.oh-my-zsh/custom/themes/lambdaurora.zsh-theme" ~/.oh-my-zsh/custom/themes
+tmp_ln "$DOTFILES_DIR/.oh-my-zsh/custom/themes/lambdaurora.zsh-theme" ~/.oh-my-zsh/custom/themes
 
 # Install configuration files for vim.
 echo "Installing vim configuration files..."
-ln -svf "$DOTFILES_DIR/.vimrc" ~
+tmp_ln "$DOTFILES_DIR/.vimrc" ~
 mkdir -p ~/.vim/colors
-ln -svf "$DOTFILES_DIR/.vim/colors/hybrid.vim" ~/.vim/colors
+tmp_ln "$DOTFILES_DIR/.vim/colors/hybrid.vim" ~/.vim/colors
 
 # Install configuration files for i3.
 echo "Installing i3 configuration files..."
 mkdir -p ~/.config/i3
-ln -svf "$DOTFILES_DIR/.config/i3/config" ~/.config/i3
+tmp_ln "$DOTFILES_DIR/.config/i3/config" ~/.config/i3
 
 # Install configuration files for polybar.
 mkdir -p ~/.config/polybar
-ln -svf "$DOTFILES_DIR/.config/polybar/config" ~/.config/polybar
-ln -svf "$DOTFILES_DIR/.config/polybar/launch.sh" ~/.config/polybar
+tmp_ln "$DOTFILES_DIR/.config/polybar/config" ~/.config/polybar
+tmp_ln "$DOTFILES_DIR/.config/polybar/launch.sh" ~/.config/polybar
+
+unalias tmp_ln
