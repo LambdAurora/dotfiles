6d0ba7df17d00c3a9f445086a4aac1b25e2069da
LambdAurora <email@lambdaurora.dev>
2022-02-27 21:27:44 +0100

    New wallpaper by @adri326.

--- .lambdadotfiles/startup/startup.sh
+++ .lambdadotfiles/startup/startup.sh
@@ -3,4 +3,4 @@
 source ~/.lambdadotfiles/startup/local/local.sh
 
 # Set wallpaper for i3.
-feh --bg-fill $HOME/.wallpapers/19-05-30--02-2.8_4k.png
+feh --bg-fill $HOME/.wallpapers/22-02-22--01-6.png
